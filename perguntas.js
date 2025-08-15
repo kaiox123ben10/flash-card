558ddd4307dab97fe0000496e200fbc9f7111aec
kaiox123ben10 <ribas.kaio@escola.pr.gov.br>
2025-08-15 09:38:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    '1-Geografia',
+    'Qual é o bioma predominante na região Norte do Brasil?',
+    'Floresta Amazônica.'
+)
+
+criaCartao(
+    '2-Geografia',
+    'Qual país possui o maior território do mundo?',
+    'Rússia.'
+)
+
+criaCartao(
+    '3-Geografia',
+    'Qual é o rio mais extenso do mundo?',
+    'Rio Nilo.'
+)
+
+criaCartao(
+    '4-Geografia',
+    'O que é latitude?',
+    'Distância, em graus, de um ponto em relação à Linha do Equador.'
+)
+
+criaCartao(
+    '5-Geografia',
+    'Qual é o deserto mais seco do mundo?',
+    'Deserto do Atacama.'
+)
+
+criaCartao(
+    '6-Geografia',
+    'Qual oceano banha a costa leste do Brasil?',
+    'Oceano Atlântico.'
+)
